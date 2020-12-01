337fff95e60ad7ee05201ce6b2d42630d42fdc5d
Dae Hee <daehi8@naver.com>
2020-12-01 12:09:09 +0900

    add DI(dependency Injection)

--- src/spring/model/DTO.java
+++ src/spring/model/DTO.java
@@ -3,6 +3,20 @@ package spring.model;
 public class DTO {
 	private String name;
 	private int num;
+
+	public DTO() {
+		System.out.println("DTO 생성");
+		this.name = "guest";
+		this.num = 20;		
+	}
+	public DTO(String name) {
+		this.name = name;
+		this.num = 20;
+	}
+	public DTO(String name, int num) {
+		this.name = name;
+		this.num = num;
+	}
 	
 	public String getName() {
 		return name;
--- src/spring/model/HelloBean.java
+++ src/spring/model/HelloBean.java
@@ -1,5 +1,6 @@
 package spring.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,10 +8,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloBean {
 	
+	// @Autowired - 컨트롤과 연결 시켜주는 어노테이션
+	// DTO를 인스턴스 변수로 선언
+	// 컨트롤러에 있는 Bean DTO객체를 @Autowired DTO에 대입
+	// DI - dependency Injection
+	// 객체를 미리 생성해 놓은 후 필요한 부분만 받아서 사용하는 형식
+	@Autowired
+	private DTO dto = null;	
+	
+	@Autowired
+	private DTO dto2 = null;	
+	
+	@Autowired
+	private DTO dto3 = null;
+	
 	// 컨트롤러의 요청이 해당 URL로 들어올경우 JSP페이지를 리턴시킨다.
 	// 처음 실행시 RequestMapping을 모두 로딩
 	@RequestMapping("/hello.do")
 	public String hello() {
+		System.out.println(dto.getName());
+		System.out.println(dto2.getName());
+		System.out.println(dto3.getName());
 		return "/WEB-INF/view/1130/hello.jsp";
 	}
 	
